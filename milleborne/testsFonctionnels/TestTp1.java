0340c4a9a31e992ff3eb45e1d50ff938fd8760b9
Cedric kalifa Lepinay bessayah <lpc3438a@ensfsi.onmicrosoft.com>
2024-02-14 17:45:34 +0100

    fin tp1

---
+++ milleborne/testsFonctionnels/TestTp1.java
@@ -0,0 +1,14 @@
+package testsFonctionnels;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestTp1 {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
